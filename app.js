ba2a3ff99ff97f951f0835d0bdde081309d7775a
askIntania <ask.intania@gmail.com>
2016-06-13 19:55:52 +0700

    Create form, accept input using post method and encoding password

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express')
+
+var multer = require('multer')
+var upload = multer({dest: 'uploads/'})
+const crypto = require('crypto');
+
+
+
 var app = express()
 app.use(express.static('public'))
 app.engine('html', require('ejs').renderFile)
 app.get('/', home)
 app.get('/register.html', register)
+app.post('/register.html', upload.fields([{ name: 'fullname', maxCount: 50 }, { name: 'email', maxCount: 50 }, { name: 'password', maxCount:20}]))
+app.post('/register.html', registerNewUser)
 app.listen(8080)
+
 
 
 function home(req, res){
@@ -13,4 +23,13 @@ function home(req, res){
 
 function register(req, res){
     res.render('register.html')
+}
+
+function registerNewUser(req, res, next){
+    console.log(req.body.password)
+    console.log(encode(req.body.password))
+    res.redirect('/')
+}
+function encode(password){
+    return crypto.createHmac('sha512', password).update('More encoding key').digest('hex')
 }
